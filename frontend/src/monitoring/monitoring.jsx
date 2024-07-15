32c2dd7c2424995c36597cf73eb234426130ec29
Bartłomiej <blesnicki@student.agh.edu.pl>
2024-07-15 12:23:05 +0200

    moniotoring fix

--- frontend/src/monitoring/monitoring.jsx
+++ frontend/src/monitoring/monitoring.jsx
@@ -3,27 +3,9 @@ import React, {useEffect, useState} from 'react';
 
 const GrafanaChart = ({ src }) => {
 
-    const [srcG, setSrcG] = useState('');
-
-    useEffect(() => {
-
-        const grafanaUrl = src;
-        const headers= {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-        };
-
-        // Możesz użyć fetch lub axios do pobrania danych
-        fetch(grafanaUrl, { headers })
-            .then(response => response.text())
-            .then(data => setSrcG(data))
-            .catch(error => console.error('Error fetching data:', error));
-    }, []);
-
     return (
         <iframe
-            srcDoc={srcG}
+            src={src}
             width="100%"
             height="600px"
             frameBorder="0"
